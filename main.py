b5633cc48279cb7e150a4f227a0a6f826305acdc
dmytro.kaminskyi92@gmail.com <dmytro.kaminskyi92@gmail.com>
2020-12-20 19:08:51 +0200

    flask start

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-def add(x, y):
-    return x + y
+from flask import Flask
 
-def diff(x, y):
-    return x - y
+import string
+import random
 
-def foo(x, y):
-    # YES
-    return x / y
+
+app = Flask(__name__)
+
+
+def generate_password():
+    choices = string.ascii_letters
+    password = ''
+
+    for i in range(10):
+        password += random.choice(choices)
+
+    return password
+
+
+@app.route('/')
+def hello_world():
+    return generate_password()
+
+
+@app.route('/exp/')
+def exp():
+    return 'exp'
+
+
+if __name__ == '__main__':
+    app.run()
